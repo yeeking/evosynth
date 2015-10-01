b2c3569c1385ec8df903a15d2bac60d9192f89bc
Matthew Yee-King <matthew@Matthews-MBP.home>
2015-10-01 08:38:10 +0100

    incremental push of new evolib

--- build_evelib.sh
+++ build_evelib.sh
@@ -1,2 +1,2 @@
-echo building lib into ./website/assets/js/evolib.js
-browserify evolib.js > ./website/assets/js/evolib.js
+echo building lib into $1
+browserify evolib.js | uglifyjs >  $1/evolib.js
---
+++ examples/basic/js/recorder.js
@@ -0,0 +1,101 @@
+// Copyright © 2013 Matt Diamond
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(function(window){
+
+  var WORKER_PATH = 'recorderWorker.js';
+
+  var Recorder = function(source, cfg){
+    var config = cfg || {};
+    var bufferLen = config.bufferLen || 4096;
+    var numChannels = config.numChannels || 2;
+    this.context = source.context;
+    this.node = (this.context.createScriptProcessor ||
+                 this.context.createJavaScriptNode).call(this.context,
+                 bufferLen, numChannels, numChannels);
+    var worker = new Worker(config.workerPath || WORKER_PATH);
+    worker.postMessage({
+      command: 'init',
+      config: {
+        sampleRate: this.context.sampleRate,
+        numChannels: numChannels
+      }
+    });
+    var recording = false,
+      currCallback;
+
+    this.node.onaudioprocess = function(e){
+      if (!recording) return;
+      var buffer = [];
+      for (var channel = 0; channel < numChannels; channel++){
+          buffer.push(e.inputBuffer.getChannelData(channel));
+      }
+      worker.postMessage({
+        command: 'record',
+        buffer: buffer
+      });
+    }
+
+    this.configure = function(cfg){
+      for (var prop in cfg){
+        if (cfg.hasOwnProperty(prop)){
+          config[prop] = cfg[prop];
+        }
+      }
+    }
+
+    this.record = function(){
+      recording = true;
+    }
+
+    this.stop = function(){
+      recording = false;
+    }
+
+    this.clear = function(){
+      worker.postMessage({ command: 'clear' });
+    }
+
+    this.getBuffer = function(cb) {
+      currCallback = cb || config.callback;
+      worker.postMessage({ command: 'getBuffer' })
+    }
+
+    this.exportWAV = function(cb, type){
+      currCallback = cb || config.callback;
+      type = type || config.type || 'audio/wav';
+      if (!currCallback) throw new Error('Callback not set');
+      worker.postMessage({
+        command: 'exportWAV',
+        type: type
+      });
+    }
+
+    worker.onmessage = function(e){
+      var blob = e.data;
+      currCallback(blob);
+    }
+
+    source.connect(this.node);
+    this.node.connect(this.context.destination);    //this should not be necessary
+  };
+
+  Recorder.forceDownload = function(blob, filename){
+    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+    var link = window.document.createElement('a');
+    link.href = url;
+    link.download = filename || 'output.wav';
+    link.target = '_blank';
+    var click = document.createEvent("Event");
+    click.initEvent("click", true, true);
+    link.dispatchEvent(click);
+  }
+
+  window.Recorder = Recorder;
+
+})(window);
